6221c31a4c59b927438db1f82db1d053e8d942d4
nosam <nosam@192.168.254.74>
2022-03-08 14:13:49 -0600

    Added User Class

---
+++ src/java/business/User.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author nosam
+ */
+public class User implements Serializable {
+    private String username, email, password; 
+    private LocalDate birthday;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, LocalDate birthday) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.birthday = birthday;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+    
+
+}
