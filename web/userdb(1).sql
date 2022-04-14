fe2058da29b7d8b441443b4184f9f6816383e451
hambo <hambo@DESKTOP-N1B74PD>
2022-04-14 14:13:12 -0500

    changed some of the profile to remove access to edit things and added time stamps to the db
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties

--- web/userdb(1).sql
+++ web/userdb(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2022 at 08:41 PM
+-- Generation Time: Apr 14, 2022 at 09:08 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -31,16 +31,17 @@ CREATE TABLE `comments` (
   `commentId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `commentText` varchar(140) NOT NULL,
-  `postId` int(11) NOT NULL
+  `postId` int(11) NOT NULL,
+  `timeStamp` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`commentId`, `userId`, `commentText`, `postId`) VALUES
-(1, 1, 'LOL!!!! Boi.', 1),
-(2, 4, 'ROFL!! My Man;)!!', 1);
+INSERT INTO `comments` (`commentId`, `userId`, `commentText`, `postId`, `timeStamp`) VALUES
+(1, 1, 'LOL!!!! Boi.', 1, '2022-04-14 14:07:22'),
+(2, 4, 'ROFL!! My Man;)!!', 1, '2022-04-14 14:07:22');
 
 -- --------------------------------------------------------
 
@@ -52,15 +53,16 @@ CREATE TABLE `posts` (
   `postId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `title` varchar(80) NOT NULL,
-  `postText` varchar(1024) NOT NULL
+  `postText` varchar(1024) NOT NULL,
+  `timeStamp` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`postId`, `userId`, `title`, `postText`) VALUES
-(4, 1, 'All about Neil Green!!!', 'Neil moved to Toronto to attend York University, earning a degree in psychology and studying theatre and film acting. He began working on film and television productions, which included big movies such as Honey starring Jessica Alba and My Baby\'s Daddy starring Michael Imperioli from The Sopranos. Neil quickly discovered that getting bit parts on large movie sets was not as fulfilling as the experience he was receiving as a lead on a smaller independent productions. It was his time on set that set him exploring and learning about the aspects of filmmaking. Soon after writing his first screenplay, Highway, it was produced by Kevin Lane of Limestyle Productions.');
+INSERT INTO `posts` (`postId`, `userId`, `title`, `postText`, `timeStamp`) VALUES
+(4, 1, 'All about Neil Green!!!', 'Neil moved to Toronto to attend York University, earning a degree in psychology and studying theatre and film acting. He began working on film and television productions, which included big movies such as Honey starring Jessica Alba and My Baby\'s Daddy starring Michael Imperioli from The Sopranos. Neil quickly discovered that getting bit parts on large movie sets was not as fulfilling as the experience he was receiving as a lead on a smaller independent productions. It was his time on set that set him exploring and learning about the aspects of filmmaking. Soon after writing his first screenplay, Highway, it was produced by Kevin Lane of Limestyle Productions.', '2022-04-20 00:00:00');
 
 -- --------------------------------------------------------
 
